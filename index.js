73b7e1700602ab4cebc9bdfbe72fcb8850ab1573
Alonso Holmes <hello@alonso.io>
2017-07-01 19:46:05 -0500

    new static audiowaveform build!

--- index.js
+++ index.js
@@ -1,23 +1,117 @@
+var request = require('request');
+var path = require('path');
+var fs = require('fs');
+var Promise = require('bluebird');
+var AWS = require('aws-sdk');
+var s3 = new AWS.S3();
+
 // See https://aws.amazon.com/blogs/compute/running-executables-in-aws-lambda/
 process.env['PATH'] = process.env['PATH'] + ':' + process.env['LAMBDA_TASK_ROOT'];
 
 var exec = require('child_process').exec;
 
+const waveformPath = '/tmp/waveform.json';
+
 // This is what Lambda will invoke to run this function
 exports.handler = (event, context, callback) => {
 
-	// First, just try to see if the thing runs
-	var cmd = './audiowaveform/audiowaveform';
+	console.info('event: ', event);
 
+	const audioUrl = event.audioUrl;
+	const pixelsPerSecond = event.pixelsPerSecond;
+	const id = event.id;
+	const bucket = event.bucket;
+
+	let start = Date.now();
+
+	downloadEpisode(audioUrl)
+	.tap(() => logTime('download', start))
+	.then(episodePath => generateWaveform(episodePath, pixelsPerSecond))
+	.tap(() => logTime('generate', start))
+	.then(() => uploadWaveform(id, bucket))
+	.tap(() => logTime('upload', start))
+	.catch(err => callback(err))
+	.finally(() => {
+		callback(null, 'OK');
+	});
+
+};
+
+const logTime = (label, start) => {
+	console.info(`[${label}] ${(Date.now() - start) / 1000} seconds`);
+	start = Date.now();
+};
+
+const downloadEpisode = audioUrl => new Promise((resolve, reject) => {
+	// Download the episode
+	console.info(`🌏 Downloading file from url: ${audioUrl}`);
+
+    // figure out the content type of this file
+    const extension = path.extname(audioUrl).split('?')[0];
+
+    // Download
+    const timestampBegin = Date.now();
+    const episodePath = `/tmp/episode${extension}`;
+    console.info({ extension, episodePath });
+    request(audioUrl)
+        .on('err', reject)
+        .on('end', res => {
+            resolve(episodePath);
+        })
+        .pipe(fs.createWriteStream(episodePath));
+});
+
+const generateWaveform = (episodePath, pixelsPerSecond) => new Promise((resolve, reject) => {
+
+	var cmd = `./audiowaveform -i ${episodePath} -o ${waveformPath} -b 8 --pixels-per-second 5`;
+	console.info('running: ' + cmd);
 	// Exec the command via node
 	exec(cmd, function(error, stdout, stderr) {
 	  // command output is in stdout
 	  if (error) {
 	  	console.error(error);
-	  	callback(error);
+	  	reject(error);
 	  } else {
-		console.log(stdout);
-		callback(null, stdout);
+	  	resolve();
 	  }
 	});
-};
+});
+
+const uploadWaveform = (id, Bucket) => new Promise((resolve, reject) => {
+	const Key = `${id}/waveform.json`
+	const Body = fs.createReadStream(waveformPath);
+	const region = 'us-west-1';
+	var s3 = new AWS.S3({
+	    apiVersion: '2006-03-01',
+	    region
+	});
+
+	console.info(`☝ Uploading ${waveformPath} ---> ${Key} in bucket ${Bucket}`);
+	s3.putObject({
+        Bucket,
+        Key,
+        Body,
+        ContentType: 'application/json',
+        ACL: 'public-read'
+    }).promise()
+    .then(res => {
+	    console.info(`Uploaded ${Key} successfully to s3!`, res);
+	    resolve(`http://s3-${region}.amazonaws.com/${Bucket}/${Key}`);
+    })
+    .catch(reject)
+});
+
+
+/**
+To test locally, 
+1. Uncomment the following
+2. Change the state of the waveform generation command from './audiowaveform' to 'audiowaveform'
+*/
+// exports.handler({
+// 	bucket: 'chorus-waveforms-dev',
+// 	id: 'new-lambda-test',
+// 	audioUrl: 'http://feeds.99percentinvisible.org/~r/99percentinvisible/~5/Kw1oEF9iEdc/199-The-Yin-and-Yang-of-Basketball-rebroadcast.mp3', 
+// 	dataPixelsPerSecond: 5
+// }, null, (err, text) => {
+// 	err ? console.error(err) : console.info(text)
+// })
